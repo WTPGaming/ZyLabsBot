a1a718871b9cf822c892acd057a8bb68ac11ff5d
Austin Poonia <63485737+WTPGaming@users.noreply.github.com>
2021-11-20 16:50:08 -0500

    Changed Solution Method

--- main.js
+++ main.js
@@ -57,9 +57,34 @@ async function getSolutions(){
 };
 
 function formatSolution(input, output){
+	var smallest = 10000;
+
+	for(i=0;i<input.length;i++){
+		if(input[i].length < smallest){
+			smallest = input[i].length
+		}
+	}
+	var resSmallest = 0;
+
+	for(a=0;a<smallest;a++){
+		var matches = false;
+		var letters = "";
+		for(i=0;i<input.length;i++){
+			if(letters.indexOf(input[i][a]) > -1){
+				matches = true
+			}
+			letters = letters + " " + input[i][a]
+		}
+		if(matches == false){
+			resSmallest = a+1;
+			break;
+		}
+	}
+
 	var solution = 'Scanner scnr = new Scanner(System.in);\nString input = scnr.nextLine();\n'
 	for(i=0;i<input.length;i++){
-		solution = solution + 'if(input.equals("'+input[i]+'")){System.out.println("'+output[i].substring(0, output[i].length - 2)+'");}\n'
+		solution = solution + 'if(input.substring(0,'+resSmallest+').equals("'+input[i].substring(0,resSmallest)+'")){System.out.println("'+output[i].substring(0, output[i].length - 2)+'");}\n'
 	}
+	//console.log(resSmallest);
 	console.log(solution);
 }
